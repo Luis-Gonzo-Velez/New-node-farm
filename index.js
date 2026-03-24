898f4db70a39c3c28b12a56584de1ac20be5f85b
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-03-24 10:35:15 -0400

    Fixing errors

--- index.js
+++ index.js
@@ -30,6 +30,21 @@ const url = require("url");
 //////////////////////////////////////////////////
 //SERVER
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8",
@@ -47,18 +62,25 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const myURL = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = myURL.pathname;
+  const query = Object.fromEntries(myURL.searchParams);
 
   //Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
     // send the contents of the template, not the literal string
-    res.end(tempOverview);
+    res.end(output);
 
     //Product page
-  } else if (pathName === "/product") {
-    // placeholder response – you can later use `dataObj` with query params
-    res.writeHead(200, { "Content-type": "text/html" });
+  } else if (pathname === "/product") {
+    console.log(query);
     res.end("This is the PRODUCT");
 
     //API
