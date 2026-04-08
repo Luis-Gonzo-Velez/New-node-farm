de29fd063cbd53a5799e64395e0c8c7b0bf75dd8
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-04-08 09:34:44 -0400

    Fixed problem on Slugify

--- index.js
+++ index.js
@@ -49,6 +49,9 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
   const myURL = new URL(req.url, `http://${req.headers.host}`);
   const pathname = myURL.pathname;
